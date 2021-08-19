5583e8092cd5dab975fd38277777876b26f2ac54
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2021-08-18 19:54:35 -1000

    Css

--- themes/inspiro-child/site-info.php
+++ themes/inspiro-child/site-info.php
@@ -10,22 +10,4 @@
 
 ?>
 <div class="site-info">
-	<?php
-	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-	}
-	?>
-	<span class="copyright">
-		<span>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'inspiro' ) ); ?>" target="_blank">
-				<?php
-				/* translators: %s: WordPress trademark */
-				printf( esc_html__( 'TESTING by %s', 'inspiro' ), 'WordPress' );
-				?>
-			</a>
-		</span>
-		<span>
-			<?php esc_html_e( 'Inspiro WordPress Theme by', 'inspiro' ); ?> <a href="<?php echo 'https://www.wpzoom.com/'; ?>" target="_blank" rel="nofollow">WPZOOM</a>
-		</span>
-	</span>
 </div><!-- .site-info -->
