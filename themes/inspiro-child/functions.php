20e58c5c5773281ffc0f428b3590c5372985ea08
Joseph Palma <43054953+josephpalma@users.noreply.github.com>
2021-08-26 14:47:18 -1000

    secure media

--- themes/inspiro-child/functions.php
+++ themes/inspiro-child/functions.php
@@ -1,6 +1,17 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles', 11 );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri() );
 }
+
+function filter_media_comment_status( $open, $post_id ) {
+    $post = get_post( $post_id );
+    if( $post->post_type == 'attachment' ) {
+        return false;
+    }
+    return $open;
+}
+add_filter( 'comments_open', 'filter_media_comment_status', 10 , 2 );
+
 ?>
